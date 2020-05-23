6635c19bed4bb40b8d8f7a73601dfd741091ce05
sam g <sghaneka@yahoo.com>
2020-05-23 15:36:27 -0400

    foo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
